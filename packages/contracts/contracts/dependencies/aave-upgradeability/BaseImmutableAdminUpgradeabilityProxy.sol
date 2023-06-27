42977ff3799e941a1293f92daeac4d679f6409f1
y <y@y.y>
2023-06-27 09:38:36 +0400

    updates from certora repo

--- packages/contracts/contracts/dependencies/aave-upgradeability/BaseImmutableAdminUpgradeabilityProxy.sol
+++ packages/contracts/contracts/dependencies/aave-upgradeability/BaseImmutableAdminUpgradeabilityProxy.sol
@@ -14,6 +14,7 @@ import "../openzeppelin/upgradeability/BaseUpgradeabilityProxy.sol";
  */
 contract BaseImmutableAdminUpgradeabilityProxy is BaseUpgradeabilityProxy {
     address immutable ADMIN;
+    uint256 FirstVaule;
 
     constructor(address _admin) {
         ADMIN = _admin;
